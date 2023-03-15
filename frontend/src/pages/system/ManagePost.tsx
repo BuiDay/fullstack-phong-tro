e735667db96599af3564e162d2512d546d8ad86f
Bui Day <buivanduynhat@gmail.com>
2023-03-16 06:46:49 +0700

    update post

--- frontend/src/pages/system/ManagePost.tsx
+++ frontend/src/pages/system/ManagePost.tsx
@@ -1,14 +1,23 @@
 import React, { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../store/hook';
 import { getPostAdmin } from '../../store/features/post/postSilce';
+import moment from 'moment';
 
 const ManagePost = () => {
     const dispatch = useAppDispatch()
     const {postsAdmin} = useAppSelector(state=>state.post)
-    console.log(postsAdmin)
     useEffect(()=>{
         dispatch(getPostAdmin())
     },[])
+
+    const checkStatus  = (datetime:any) =>{
+        console.log(datetime)
+        // let todayInSecond = new Date().getTime();
+        // let expireDayInSeconds = datetime.getTime();
+        // console.log(todayInSecond, expireDayInSeconds)
+        // return todayInSecond <= expireDayInSeconds ? " Đã hết hạn" : "Đang hoạt động"
+    }
+
     return (
         <>
             <div className='py-4 border-b border-gray-200 flex items-center justify-between'>
@@ -20,27 +29,31 @@ const ManagePost = () => {
             <table className='w-full'>
                 <thead>
                     <tr>
-                        <th>Mã tin</th>
-                        <th>Ảnh đại diện</th>
-                        <th>Tiêu đề</th>
-                        <th>Giá</th>
-                        <th>Ngày bắt đầu</th>
-                        <th>Ngày hết hạn</th>
-                        <th>Trạng thái</th>
+                        <th className='py-3 border'>Mã tin</th>
+                        <th className='py-3 border'>Ảnh đại diện</th>
+                        <th className='py-3 border'>Tiêu đề</th>
+                        <th className='py-3 border'>Giá</th>
+                        <th className='py-3 border'>Ngày bắt đầu</th>
+                        <th className='py-3 border'>Ngày hết hạn</th>
+                        <th className='py-3 border'>Trạng thái</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
                         postsAdmin && postsAdmin.map((item:any)=>{
                             return (
-                            <>
-                                <tr key={item.id}>
-                                    <td>#{item.id.split("-")[0]}</td>
-                                    <td><img className='h-[50px]' src={JSON.parse(item.images.image)[0]} alt="" /></td>
-                                    <td>{item.title}</td>
-                                    <td>{item.attributes.price}</td>
-                                </tr>  
-                            </>)
+                                <>
+                                    <tr key={item.id} className='text-sm text-center'>
+                                        <td className='py-1 border'>#{item.id.split("-")[0]}</td>
+                                        <td className='flex justify-center py-1 border'><img className='h-[50px]' src={JSON.parse(item.images.image)[0]} alt="" /></td>
+                                        <td className='text-left py-1 px-3 border'>{item.title}</td>
+                                        <td className='py-1 border'>{item.attributes.price}</td>
+                                        <td className='py-1 border'>{item.overviews.created}</td>
+                                        <td className='py-1 border'>{item.overviews.expired}</td>
+                                        <td className='py-1 border'>{item.overviews.expired && checkStatus(new Date())}</td>
+                                    </tr>  
+                                </>
+                            )
                         })
                     }
                 </tbody>
